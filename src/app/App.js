1116e6e788c5962c8a62bf9bb81d3c2aedc3edff
Carles E <carles@ganxo.net>
2023-09-15 23:19:15 +0200

    Changed theme colors

--- src/app/App.js
+++ src/app/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Counter } from '../features/counter/Counter';
 import './App.module.css';
 import NavBar from '../components/NavBar/NavBar';
+import {tema} from './theme';
+import { ThemeProvider } from '@mui/material';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavBar />
+        <ThemeProvider theme={tema}>
+          <NavBar />
+        </ThemeProvider>
       </header>
     </div>
   );
---
+++ src/app/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material"
+
+
+export const tema = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#ff3d00',
+    },
+    secondary: {
+      main: '#78909c',
+    },
+  }
+});
