a56251c93d6a27e38bc567ee5d3991b96ded27cd
Carles E <carles@ganxo.net>
2023-09-18 10:43:20 +0200

    Added comments button

--- src/components/TopicCard/TopicCard.js
+++ src/components/TopicCard/TopicCard.js
@@ -1,5 +1,8 @@
-import { Avatar, Card, CardHeader, CardMedia } from "@mui/material";
+import { Avatar, Button, ButtonGroup, Card, CardActions, CardHeader, CardMedia, IconButton, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import ArrowUpwardOutlinedIcon from '@mui/icons-material/ArrowUpwardOutlined';
+import ArrowDownwardOutlinedIcon from '@mui/icons-material/ArrowDownwardOutlined';
+import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
 
 export default function TopicCard(props) {
   const [data, setData] = useState();
@@ -29,6 +32,24 @@ export default function TopicCard(props) {
         {data && data.thumbnail && (
             <CardMediaProc />
         )}
+        <CardActions>
+            <ButtonGroup variant="contained" sx={{
+                borderRadius: "20px",
+                height: "40px",
+                alignItems: "center",
+            }}>
+                <IconButton><ArrowUpwardOutlinedIcon/></IconButton>
+                <Typography>{data.ups}</Typography>
+                <IconButton><ArrowDownwardOutlinedIcon/></IconButton>
+            </ButtonGroup>
+            <Button variant="contained" sx={{
+                borderRadius: "20px",
+                height: "40px",
+            }}>
+                <Typography>{data.num_comments}</Typography>
+                <ChatOutlinedIcon />
+            </Button>
+        </CardActions>
       </Card>
     </div>
   );
